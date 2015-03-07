7ac98b0e591c2248a0de364a5c5ba83355aaa745
mlondi99 <perfectmadondo@gmail.com>
2015-03-07 19:16:38 +0200

    fraction.h header file

---
+++ ConsoleApplication2/Test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include "fraction.h"
+
+int main()
+{
+	fraction f1, f2, f3, f4, f5, f6;
+	cout << "The results displayed will be the sum, difference, product and quotient.\n " << endl;
+	f1 = getFrac();
+	f2 = getFrac();
+	f3 = f1.addFraction(f2); //Adds the two fractions;
+	f3.lowterms(); //Takes the result and reduces it
+	f3.print();
+	f4 = f1.subFraction(f2); //Subtracts the two fractions
+	f4.lowterms(); //Takes the result and reduces it
+	f4.print();
+	f5 = f1.mulFraction(f2); //Multiplies the two fractions
+	f5.lowterms(); //Takes the result and reduces it
+	f5.print();
+	f6 = f1.divFraction(f2); //Divides the two fractions
+	f6.lowterms(); //Takes the result and reduces it
+	f6.print();
+	system("pause");
+	return 0;
+}
---
+++ ConsoleApplication2/fraction.h
@@ -0,0 +1,29 @@
+#ifndef fraction_H
+#define fraction_H
+
+class fraction
+{
+private:
+	int num;
+	int den;
+	char dummychar; //A variable to hold the “/” character, thrown away at the end
+public:
+
+	fraction(int numerator = 0, int denominator = 1);
+	~fraction();
+
+	fraction addFraction(fraction add);
+	fraction subFraction(fraction sub);
+	fraction mulFraction(fraction mul);
+	fraction divFraction(fraction div);
+
+	fraction operator+(fraction add);
+	fraction operator-(fraction sub);
+	fraction operator*(fraction mul);
+	fraction operator/(fraction div);
+
+	void fraction::lowterms();
+	void print();
+};
+fraction getFrac();
+#endif
